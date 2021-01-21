8b11184d09a50b794c6d6ba2f729b40b02cbdd26
didrlgus <rlgusdid@naver.com>
2021-01-21 15:12:16 +0900

    webflux msa 멀티프로젝트 구성 완료

---
+++ microservices/recommendation-service/src/main/java/microservices/core/recommendataion/RecommendationServiceApplication.java
@@ -0,0 +1,12 @@
+package microservices.core.recommendataion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
+}
